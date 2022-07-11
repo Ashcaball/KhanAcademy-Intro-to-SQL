13707476fc2e59faae4769e298f58b6b6e41ea01
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-10 22:30:18 -0400

    Completed Coursework

--- Challenge1_Book_list.sql
+++ Challenge1_Book_list.sql
@@ -1,8 +1,5 @@
-/*Challenge: Book list database
-Step 1
-Create a table to store a list of books. It should have columns for id, name, and rating. 
-Add three of your favorite books into the table.
-Answer */
+/* Create a table to store a list of books. It should have columns for id, name, and rating. 
+Add three of your favorite books into the table. */
 
 INSERT INTO Books VALUES (1, "1984", 5);
 INSERT INTO Books VALUES (2, "To kill a mockingbird", 3);
