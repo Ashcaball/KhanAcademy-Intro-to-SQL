c233bc16ccd60d10246eebba79a7371aaecbf90a
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-10 22:29:37 -0400

    Completed Coursework

--- Challenge10_Sequels_in_SQL.sql
+++ Challenge10_Sequels_in_SQL.sql
@@ -1,4 +1,5 @@
-/* We've created a table with all the 'Harry Potter' movies, with a sequel_id column that matches the id of the sequel for each movie. Issue a SELECT that will show the title of each movie next to its sequel's title (or NULL if it doesn't have a sequel). */
+/* We've created a table with all the 'Harry Potter' movies, with a sequel_id column that matches the id of the sequel for each movie. 
+Issue a SELECT that will show the title of each movie next to its sequel's title (or NULL if it doesn't have a sequel). */
 
 CREATE TABLE movies (id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
