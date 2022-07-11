3bfd9fc02ee8dd670f483a2e9aa78bc1d324f883
Ashley Caballero <106620855+Ashcaball@users.noreply.github.com>
2022-07-10 21:58:49 -0400

    Completed Coursework

--- Challenge1_Book_list.sql
+++ Challenge1_Book_list.sql
@@ -4,6 +4,6 @@ Create a table to store a list of books. It should have columns for id, name, an
 Add three of your favorite books into the table.
 Answer */
 
-INSERT INTO Books VALUES (1, "You are a badass", 5);
+INSERT INTO Books VALUES (1, "1984", 5);
 INSERT INTO Books VALUES (2, "To kill a mockingbird", 3);
-INSERT INTO Books VALUES (3, "HAHAHAHA", 5);
+INSERT INTO Books VALUES (3, "Lovely Bones", 5);
